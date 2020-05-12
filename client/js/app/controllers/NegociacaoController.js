55c2ee5d857a21d337f9aca52b660f10c73a1ba9
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-12 13:29:24 -0300

    aula 1 completa

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,9 +7,11 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
-    this._listaNegociacoes = new ListaNegociacoes();
+    this._listaNegociacoes = new ListaNegociacoes(model =>
+      this._negociacoesView.update(model));
+
+
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-    
     this._negociacoesView.update(this._listaNegociacoes);
     
     this._mensagem = new Mensagem();
@@ -20,12 +22,18 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._negociacoesView.update(this._listaNegociacoes);
 
     this._mensagem.texto = 'Negociação adicionada com sucesso!';
     this._mensagemView.update(this._mensagem);
     
     this._limpaFormalario();
+  }
+
+  apaga() {
+    this._listaNegociacoes.esvazia();
+
+    this._mensagem.texto = 'Negociações apagadas com sucesso';
+    this._mensagemView.update(this._mensagem)
   }
 
   _criaNegociacao() {
